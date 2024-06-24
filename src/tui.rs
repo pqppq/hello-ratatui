4e26edbedd49d438290fdcb099b9231ece945d6f
pqppq <yutatsu0119@gmail.com>
2024-06-24 23:39:10 +0900

    feat: counting app

---
+++ src/tui.rs
@@ -0,0 +1,29 @@
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    symbols::border,
+    widgets::Paragraph,
+    widgets::{block::*, *},
+};
+use std::io::{self, stdout, Result, Stdout};
+
+use crossterm::{execute, terminal::*};
+use ratatui::prelude::*;
+
+pub type Tui = Terminal<CrosstermBackend<Stdout>>;
+
+pub fn init() -> io::Result<Tui> {
+    execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    Terminal::new(CrosstermBackend::new(stdout()))
+}
+
+pub fn restore() -> io::Result<()> {
+    execute!(stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode();
+    Ok(())
+}
